a379b3778961d775cc1e9450e9b04afd70724851
Siobhan <spowell2@stanford.edu>
2019-02-22 12:48:34 -0800

    Made specific changes from code review meeting, still need to do formatting changes. Also deleted method process_data since the data normalization is now done as part of the sklearn training and doesnt need to be done manually anymore.

--- mlpowerflow/mlpf.py
+++ mlpowerflow/mlpf.py
@@ -1,4 +1,3 @@
-# import mlpf -- contains all functions of machine learning powerflow project
 
 import numpy as np
 import sklearn
@@ -11,20 +10,18 @@ from sklearn.metrics.pairwise import polynomial_kernel
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
 
-class forward_mlpf:
+class Forward_MLPF:
     
     # ML Powerflow Forward Mapping
     
     def __init__(self, numbus):
-        self.numbus           = numbus # The total number of nodes in the network
-        self.itworked         = 'Initialized!'
+        self.numbus = numbus 
         self.p = np.zeros((1,1))
         self.q = np.zeros((1,1))
         self.v = np.zeros((1,1))
         self.a = np.zeros((1,1))
         self.uw = np.zeros((1,1))
-        self.uw_norm = np.zeros((1,1))
-        self.pq_norm = np.zeros((1,1))
+        self.pq = np.zeros((1,1))
         self.X_train = np.zeros((1,1))
         self.y_train = np.zeros((1,1))
         self.X_test = np.zeros((1,1))
@@ -33,12 +30,18 @@ class forward_mlpf:
         self.Ntest = 0
         self.N = 0 # Total number in data set
         self.n = 0
-#         self.modelcounter     = 0   # Number of source signals
-#         self.models           = {}  # Model for each source signal, this is a list but could be a dict
-#         self.constraints      = []  # Additional constraints
-#         self.N                = len(aggregateSignal) # Length of aggregate signal
+        self.test_y_vals = np.zeros((1,1))
+        self.test_error_vals = np.zeros((1,1))
+        self.total_rmse = np.zeros((1,1))
+        self.b = np.zeros((1,1))
+        self.coeffs = np.zeros((1,1))
+        self.num_SV = np.zeros((1,1))
+        self.SV_inds = np.zeros((1,1))
+        self.C_best = np.zeros((1,1))
+        self.eps_best = np.zeros((1,1))
 
-
+        
+        
     def supply_full_data(self, p, q, v, a, n, N):
         
         # Store the data
@@ -52,71 +55,31 @@ class forward_mlpf:
         u = v * np.cos(a) # Make sure a is in radians
         w = v * np.sin(a)
         uw = np.zeros((N,2*n))
+        uw[:,np.arange(0,n)] = u
+        uw[:,np.arange(n,2*n)] = w
+        pq = np.zeros((N,2*n))
+        pq[:,np.arange(0,n)] = p
+        pq[:,np.arange(n,2*n)] = q
         
         self.uw = np.copy(uw)
+        self.pq = np.copy(pq)
         self.N = N
         self.n = n
         
         
-        
-    def process_data(self,THRESHOLD):
-
-        # Process the data ---- Replace with standardscaler
-        uw_mean = np.mean(self.uw,axis=0)
-        uw_std = np.std(self.uw,axis=0)
-        p_mean = np.mean(self.p,axis=0)
-        p_std = np.std(self.p,axis=0)
-        q_mean = np.mean(self.q,axis=0)
-        q_std = np.std(self.q,axis=0)
-        
-        N = self.N
-        n = self.n
-        uw_norm = np.zeros((N,2*n))
-        pq_norm = np.zeros((N,2*n))
-#         q_norm = np.zeros((N,n))
-        for j in range(n):
-            if uw_std[j] != 0:
-                uw_norm[:,j] = ( self.uw[:,j] - uw_mean[j] ) / uw_std[j]
-            else: 
-                uw_norm[:,j] = ( self.uw[:,j] - uw_mean[j] )
-            if p_std[j] != 0:
-                pq_norm[:,j] = ( self.p[:,j] - p_mean[j] ) / p_std[j]
-            else: 
-                pq_norm[:,j] = ( self.p[:,j] - p_mean[j] )
-            if q_std[j] != 0:
-                pq_norm[:,j+n] = ( self.q[:,j] - q_mean[j] ) / q_std[j]
-            else: 
-                pq_norm[:,j+n] = ( self.q[:,j] - q_mean[j] )
-            if uw_std[j+n] != 0:
-                uw_norm[:,j+n] = ( self.uw[:,j+n] - uw_mean[j+n] ) / uw_std[j+n]
-            else: 
-                uw_norm[:,j+n] = ( self.uw[:,j+n] - uw_mean[j+n] )
-            
-        self.uw_norm = np.copy(uw_norm)
-        self.pq_norm = np.copy(pq_norm)
-#         self.q_norm = np.copy(q_norm)
-        
-#         # Only train on columns where the variability is still significant after normalization
-#         training_cols = []
-#         for col in range(n):
-#             if abs(uw_mean[col]) > THRESHOLD:
-#                 training_cols.append(col)
-                
-#         self.training_cols = training_cols
-        
     def train_test_split_data(self, rand_percent = True, train_percent = 0.8, time_sers = False, time_percent = 0.8):
         
-        if rand_percent == True:
-            X_train, X_test, y_train, y_test = train_test_split(self.uw_norm, self.pq_norm, train_size=train_percent, random_state=42)
+        if rand_percent:
+            X_train, X_test, y_train, y_test = train_test_split(self.uw, self.pq, train_size=train_percent, random_state=42)
             Ntrain = np.shape(X_train)[0]
             Ntest = np.shape(X_test)[0]
-        if time_sers == True:
-            Ntrain = int(time_percent*np.shape(self.uw_norm)[0])
-            Ntest = np.shape(self.uw_norm)[0] - Ntrain
-            X_train = self.uw_norm[np.arange(0,Ntrain),:]
-            X_test = self.uw_norm[np.arange(Ntrain,Ntrain+Ntest),:]
-            y_train = self.pq_norm[np.arange(0,Ntrain),:]
-            y_test = self.pq_norm[np.arange(Ntrain,Ntrain+Ntest),:]
+        if time_sers:
+            Ntrain = int(time_percent*np.shape(self.uw)[0])
+            Ntest = np.shape(self.uw)[0] - Ntrain
+            X_train = self.uw[np.arange(0,Ntrain),:]
+            X_test = self.uw[np.arange(Ntrain,Ntrain+Ntest),:]
+            y_train = self.pq[np.arange(0,Ntrain),:]
+            y_test = self.pq[np.arange(Ntrain,Ntrain+Ntest),:]
             
         self.X_train = np.copy(X_train)
         self.X_test = np.copy(X_test)
@@ -124,8 +87,6 @@ class forward_mlpf:
         self.y_test = np.copy(y_test)
         self.Ntrain = Ntrain
         self.Ntest = Ntest
-    
-        return
     
     
     def fit_svr(self, C_set, eps_set, maxiter):
@@ -166,13 +127,12 @@ class forward_mlpf:
                 coeffs[j,regr_svr.support_[i]] = vals.item(i) 
                 SV_inds[j,regr_svr.support_[i]] = 1 
                 
-            self.b = b
-            self.coeffs = coeffs
-            self.num_SV = num_SV
-            self.SV_inds = SV_inds
-            self.C_best = C_best
-            self.eps_best = eps_best
-            self.regressor = regr_svr
+        self.b = b
+        self.coeffs = coeffs
+        self.num_SV = num_SV
+        self.SV_inds = SV_inds
+        self.C_best = C_best
+        self.eps_best = eps_best
             
             
     def apply_svr(self, X_sample):
@@ -197,7 +157,7 @@ class forward_mlpf:
 
         for i in range(self.Ntest):
 
-            test_y_vals[i,:] = forward_mlpf.apply_svr(self,self.X_test[i,:])
+            test_y_vals[i,:] = self.apply_svr(self.X_test[i,:])
             test_error_vals[i] = np.linalg.norm(test_y_vals[i,:]-self.y_test[i,:],2)/np.power(self.n,0.5)
 
         total_rmse = np.sqrt(np.mean(np.power(test_y_vals - self.y_test,2)))
